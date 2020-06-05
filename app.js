7b36ec596d6042c87a6ad68348da2a98de77cf4a
dhimasadip <dhimasadip.dap@gmail.com>
2020-06-05 08:17:25 +0700

    add readme

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const routes = require('./routes')
-const port = 3000
+const port = process.env.PORT || 4000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
